cd06a24c0201a57de3bb598c0769c527f0e23ecd
Aaron Lim <Aaron Lim>
2015-11-25 14:17:30 +0800

    My first commit

---
+++ src/com/tutorial/jenkins/JenkinsMain.java
@@ -0,0 +1,10 @@
+package com.tutorial.jenkins;
+
+public class JenkinsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Jenkins Project v2");
+	}
+
+}
